47eb1543cbd44bea0a05ad0430970129b30c5161
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-06-02 16:49:58 +0900

    fix lint

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
-import { MessageService } from './message.service'
+import { MessageService } from './message.service';
 
 import { Hero } from './hero';
 import {HEROES} from './mock-heroes';
@@ -17,6 +17,6 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
 
-    return of(HEROES)
+    return of(HEROES);
   }
 }
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service'
+import { HeroService } from '../hero.service';
 
 import { Hero } from '../hero';
 
