0225c5d8db3e063db06c050b514dac17dd3260fc
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-06-02 15:39:50 +0900

    ヒーローリストの追加、 *ngFor *ngIf, [class.hogehoge] の学習

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 
+import {HEROES} from '../mock-heroes';
+
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  selectedHero: Hero;
+  heroes: Hero[] = HEROES;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Okada'},
+  { id: 2, name: 'Genya'},
+  { id: 3, name: 'Fuu'},
+  { id: 4, name: 'Calsifar'}
+]
