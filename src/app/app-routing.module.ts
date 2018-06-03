4a7384c87d598cf20ffec9992d13942763306be3
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-06-04 00:09:57 +0900

    add dashboard style & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HerosComponent } from './heros/heros.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  {path: 'heroes', component: HerosComponent}
-]
+  {path: 'heroes', component: HerosComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,5 +4,6 @@ export const HEROES: Hero[] = [
   { id: 1, name: 'Okada'},
   { id: 2, name: 'Genya'},
   { id: 3, name: 'Fuu'},
-  { id: 4, name: 'Calsifar'}
+  { id: 4, name: 'Calsifar'},
+  { id: 5, name: 'Nekko'}
 ];
