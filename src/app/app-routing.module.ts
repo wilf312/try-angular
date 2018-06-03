337f5f9cf7545ddc50bd37a960e53313add32a45
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-06-03 23:32:06 +0900

    add route and add nav into app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HerosComponent } from './heros/heros.component';
+
+const routes: Routes = [
+  {path: 'heroes', component: HerosComponent}
+]
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
